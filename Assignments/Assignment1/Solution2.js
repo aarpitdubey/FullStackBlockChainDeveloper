4593d1f2c52fdfac0bf0dfa1412ffba95e46ee50
aarpitdubey <aarpitdubey@gmail.com>
2022-09-06 11:57:37 +0530

    Assignment 1  Added to repository

--- Assignments/Assignment1/Solution2.js
+++ Assignments/Assignment1/Solution2.js
@@ -6,6 +6,13 @@
 // 60°C is 140 °F
 // 45°F is 7.222222222222222°C
 
+// Steps:
+// 1. Create two Fuctions for returning Fahrenheit value and for returning Celsius value.
+// 2. fuctions accepting the arguments in celsius and fahrenheit respectively
+// 3. logics:
+// Formula : c/5 = (f-32)/9
+// 4. we have to manipulate the same formula in terms of Fahrenheit and Celsius
+
 // 1. 60°C is 140 °F
 let Fahrenheit = (c)=>{
     f = (9 * (c/5)) + 32;
