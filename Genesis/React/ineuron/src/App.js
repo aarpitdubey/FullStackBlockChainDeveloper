8a7c3350b44d67900f64a43c41f2e6c89cce62d3
aarpitdubey <aarpitdubey@gmail.com>
2022-12-28 23:00:07 +0530

    Added new react folder

---
+++ Genesis/React/ineuron/src/App.js
@@ -0,0 +1,28 @@
+// import logo from './logo.svg';
+import './App.css';
+import Welcome from './component/Arpit';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Arpit Dubey is a Software Engineer and Web Developer working at ISDC Global.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+  </header> */}
+
+      <Welcome />
+    </div>
+  );
+}
+
+export default App;
---
+++ Genesis/React/ineuron/src/component/Arpit.js
@@ -0,0 +1,9 @@
+import React from 'react'; // necessary files 
+
+
+function Welcome() 
+{
+    return <h1>React is best</h1>
+}
+
+export default Welcome;
